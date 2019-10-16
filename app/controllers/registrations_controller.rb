116c2b4d10c3a936afd786dc65142f0f88000587
jay.gao <jay.gao@alo7.com>
2019-10-16 21:24:32 +0800

    feat: overwrite after sign up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
   class RegistrationsController < Devise::RegistrationsController
     # before_action :configure_sign_up_params, only: [:create]
 
-    # protected
+    protected
+
+    def after_sign_up_path_for(resource)
+      Rails.env.production? ? 'ttnote.cn/login?emailConfirmed' : 'beta.ttnote.cn/login?emailConfirmed'
+    end
+
     # # If you have extra params to permit, append them to the sanitizer.
     # def configure_sign_up_params
     #   devise_parameter_sanitizer.permit(:sign_up, keys: [])
