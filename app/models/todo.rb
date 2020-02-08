cf1f2fd09eba48cf647119553771d707d6ab8023
jay.gao <jay.gao@alo7.com>
2020-02-08 15:23:24 +0800

    Feat: remove redis today todos after todo destroy

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,6 +4,8 @@ class Todo < ApplicationRecord
   has_many :tomatoes, dependent: :destroy
   has_one :today_todo
 
+  after_destroy :remove_redis_today_todos_id
+
   def load_tomatoes_lazily
     BatchLoader.for(self.id).batch(default_value: []) do |todo_ids, loader|
       Tomato.where(todo_id: todo_ids).each do |tomato|
@@ -23,4 +25,11 @@ class Todo < ApplicationRecord
     end
   end
 
+  private
+  def remove_redis_today_todos_id
+    user_id = self.project.user_id
+    todo_ids_key = "today_todos:#{user_id}:todo_ids"
+    redis = REDIS
+    redis.lrem(todo_ids_key, 0, self.id)
+  end
 end
